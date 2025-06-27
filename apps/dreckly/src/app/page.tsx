34a87986e86e7f409055303c6ba0e6a7f25b2c56
James Husband <jchusband@proton.me>
2025-06-27 19:12:02 +0100

    wiring up

--- apps/dreckly/src/app/page.tsx
+++ apps/dreckly/src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Index() {
+import { UiKit } from '@dreckly/ui-kit';
+import { User } from '@dreckly/types';
+
+export default async function Index() {
+  // get from api
+  const response = await fetch('http://localhost:3000/api/hello');
+
+  const dummyUser: User = {
+    id: '1',
+    name: 'John Doe',
+    email: 'john@example.com',
+  };
+
+  console.log(response);
+
   return (
     <div>
-      <h1>Dreckly</h1>
+      <p>{dummyUser.name}</p>
+      <UiKit />
     </div>
   );
 }
