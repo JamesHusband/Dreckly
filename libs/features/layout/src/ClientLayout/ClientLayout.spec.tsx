90045c42fd7ebe7a3d114eb5d269027470dd7f95
James Husband <jchusband@proton.me>
2025-06-27 20:08:49 +0100

    add feature lib: ClientLayout

---
+++ libs/features/layout/src/ClientLayout/ClientLayout.spec.tsx
@@ -0,0 +1,8 @@
+import ClientLayout from './ClientLayout';
+import { render } from '@testing-library/react';
+
+describe('ClientLayout', () => {
+  it('should render', () => {
+    render(<ClientLayout>Hello</ClientLayout>);
+  });
+});
