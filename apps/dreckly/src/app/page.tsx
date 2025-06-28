9cb02986835edbc338d8920e802706ab4e731553
James Husband <jchusband@proton.me>
2025-06-28 08:59:10 +0100

    remove boilerplate

--- apps/dreckly/src/app/page.tsx
+++ apps/dreckly/src/app/page.tsx
@@ -1,10 +1,7 @@
-import { getUser } from '@dreckly/data-access';
 import { CuisineFilter, CuisineProvider, Hero } from '@dreckly/home';
 import { RestaurantList } from '@dreckly/restaurants';
 
 export default async function Index() {
-  const user = await getUser();
-
   return (
     <CuisineProvider>
       <Hero />
@@ -14,7 +11,6 @@ export default async function Index() {
           <RestaurantList />
         </div>
       </section>
-      <p>{user.name}</p>
     </CuisineProvider>
   );
 }
